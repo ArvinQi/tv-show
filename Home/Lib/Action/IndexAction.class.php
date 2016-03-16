da8c3d1a40950f05348e2e416c0beaa166c3c148
Jaylon <563968247@qq.com>
2016-03-16 14:48:37 +0800

    over

--- Home/Lib/Action/IndexAction.class.php
+++ Home/Lib/Action/IndexAction.class.php
@@ -8,13 +8,20 @@ class IndexAction extends Action {
         $da=array();
         $da['category'] = array();
         $da['data'] = array();
-		    $inte=M('supplier_log');
-        $data = $inte->query("SELECT SUM(sup) sum, DATE_FORMAT(FROM_UNIXTIME( `regTime`),'%y年%m月') sdate  FROM `j_supplier_log` GROUP BY sdate");
+		    $supplier_log=M('supplier_log');
+        $data = $supplier_log->query("SELECT SUM(sup) sum, DATE_FORMAT(FROM_UNIXTIME( `regTime`),'%y年%m月') sdate FROM `j_supplier_log` where regTime>1451577600 and supFrom like '%导入%' GROUP BY sdate");
         foreach($data as $key=>$va){
           array_push($da['category'],$va[sdate]);
           array_push($da['data'],$va[sum]);
 
         }
 				echo json_encode($da);
+    }
+    public function inteRT(){
+        $timestamp = $_GET['time']? $_GET['time'] : time(date('Y-m-d'));
+        $supplier_log=M('supplier_log');
+        $data = $supplier_log->query("SELECT SUM(sup) SUM FROM j_supplier_log WHERE regTime > ".$timestamp." AND supFrom NOT LIKE '%导入%' AND supFrom NOT LIKE '%测试%'");
+
+        echo json_encode($data);
     }
 }
